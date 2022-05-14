b663fae6e0f10639d46e5072113cf2649ec7829d
moskvin <moskvin27@gmail.com>
2022-05-14 15:39:27 +0300

    work_with_window

---
+++ selenium/2.3 work_with_window/2.3 auto_test_four.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+import math
+
+
+# def calc(x):
+#     return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    link = 'http://suninjuly.github.io/alert_accept.html'
+    browser = webdriver.Chrome()
+    browser.get(link)
+    browser.find_element_by_css_selector('button.btn').click()
+    browser.switch_to.alert.accept()
+
+    num = browser.find_element_by_id('input_value').text
+    num_input = str(math.log(abs(12 * math.sin(int(num)))))
+    answer = browser.find_element_by_id('answer')
+    answer.send_keys(num_input)
+
+    browser.find_element_by_css_selector('button.btn').click()
+
+finally:
+    time.sleep(10)
+    browser.quit()
